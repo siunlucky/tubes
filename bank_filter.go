1af1315022e50724c95f3451197e88a0a685e7ea
Faiz Elfahad Kurniawan <74404911+siunlucky@users.noreply.github.com>
2024-06-03 21:11:05 +0700

    #4

--- bank_filter.go
+++ bank_filter.go
@@ -1,10 +1,24 @@
 package main
 
 func searchBankByUniqueCode(search int) int {
-	for i := 0; i < worldBank.nBank; i++ {
-		if search == worldBank.Banks[i].uniqueCode {
-			return i
+	// Binary Search sorted by func sortBankByUniqueCode
+	var left, right, mid int
+
+	sortBankByUniqueCode(&worldBank, "asc")
+
+	// Binary Search
+	left = 0
+	right = worldBank.nBank - 1
+	for left <= right {
+		mid = left + (right-left)/2
+		if worldBank.Banks[mid].uniqueCode == search {
+			return mid
+		} else if worldBank.Banks[mid].uniqueCode < search {
+			left = mid + 1
+		} else {
+			right = mid - 1
 		}
 	}
+
 	return -1
 }
--- bank_sort.go
+++ bank_sort.go
@@ -1,1 +1,22 @@
 package main
+
+func sortBankByUniqueCode(worldBank *WorldBank, direction string) {
+	var i, j int
+	for i = 0; i < worldBank.nBank; i++ {
+		for j = i + 1; j < worldBank.nBank; j++ {
+			if direction == "asc" {
+				if worldBank.Banks[i].uniqueCode > worldBank.Banks[j].uniqueCode {
+					temp := worldBank.Banks[i]
+					worldBank.Banks[i] = worldBank.Banks[j]
+					worldBank.Banks[j] = temp
+				}
+			} else {
+				if worldBank.Banks[i].uniqueCode < worldBank.Banks[j].uniqueCode {
+					temp := worldBank.Banks[i]
+					worldBank.Banks[i] = worldBank.Banks[j]
+					worldBank.Banks[j] = temp
+				}
+			}
+		}
+	}
+}
